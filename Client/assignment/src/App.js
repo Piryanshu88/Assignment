22160df48c97f8bc0fcee8346cb31790ded8cb25
Piryanshu88 <deepubisht2004@gmail.com>
2023-05-29 12:14:38 +0530

    adding fetch post method

--- Client/assignment/src/App.js
+++ Client/assignment/src/App.js
@@ -11,20 +11,48 @@ let arr = [
 ];
 function App() {
   const handleClick = (tag) => {
+    const video = document.querySelector("#video");
+    video.pause();
+    let videoTime = video.currentTime;
+    videoTime = (videoTime / 60).toFixed(2);
     const data = {
       tags: tag,
       Date: new Date(),
-      videoId: "assignment",
-      videoTime: "",
+      videoId: "demo-id",
+      videoTime,
     };
-    console.log("data", data);
+
+    fetch(`http://localhost:8080/add`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((re) => re.json())
+      .then((re) => {
+        console.log("Data Added Succesfully", re);
+        const toast = document.querySelector(".toast");
+        toast.innerText = "Data Send Successfully";
+        toast.style.display = "block";
+        setTimeout(() => {
+          toast.innerText = "";
+          toast.style.display = "none";
+        }, 2000);
+        video.play();
+      })
+      .catch((err) => console.log(err));
   };
   return (
     <div className="App">
       <h2>Assignment</h2>
-      <video width="750" height="500" controls>
-        <source src="https://youtu.be/buRG1eGuPIs" type="video/mp4" />
-      </video>
+      <video
+        src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+        width="750"
+        height="500"
+        controls
+        id="video"
+      ></video>
       <div className="tags">
         {arr.map((el, i) => {
           return (
@@ -34,6 +62,7 @@ function App() {
           );
         })}
       </div>
+      <div className="toast"></div>
     </div>
   );
 }
